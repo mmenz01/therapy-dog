43261b962ceff25209991e58d707712fb7d4dfae
mmenz01 <77908171+mmenz01@users.noreply.github.com>
2021-01-23 22:04:52 -0500

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,7 @@
+document.getElementById("button").addEventListener("click", changeImage);
+function changeImage()
+        {
+        var img = document.getElementById("image");
+        img.src="heartdog.jpeg";
+        return false;
+        }
